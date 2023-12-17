e9f844f662bb909d0f3cd44a895bd192fa0e5c92
erikgamez23 <erikgamez23@gmail.com>
2023-12-17 15:16:30 -0500

    Start SwiftUI APp

---
+++ STA/STA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  STA
+//
+//  Created by Erik Gamez on 12/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ STA/STA/STAApp.swift
@@ -0,0 +1,17 @@
+//
+//  STAApp.swift
+//  STA
+//
+//  Created by Erik Gamez on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct STAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
